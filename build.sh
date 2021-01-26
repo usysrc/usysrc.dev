e4584f3ced09b833cd2bc51a97a29276f1e33778
usysrc <tilmann.hars@googlemail.com>
2021-01-26 23:45:02 +0100

    building refined

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sed '/content/{
-    s/content//g
-    r index_raw.html
-}' template.html
+cp -r img ./dist/
+cp -r style ./dist/style/
+
+for filename in ./pages/*.html; do
+echo $filename
+    # filename=$(basename $filename)
+    cat header.html $filename footer.html > ./dist/$(basename $filename)
+done
