ef7a049b57661a82052c7d16b5ce0822bed1d4a9
usysrc <tilmann.hars@googlemail.com>
2021-01-26 23:34:02 +0100

    header and footer

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+sed '/content/{
+    s/content//g
+    r index_raw.html
+}' template.html
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+#
+# upload via ftp
+# ...all inkl only has ftp in the lowest host tier...
+#
 
 source .env
 
 ftp ftp://$user:$password@$server <<END_SCRIPT
 prompt
 mput *.html
-mput style
-mput img
+lcd style
+cd style
+mput *
+lcd ../style
+cd ../img
+mput *
 exit
 END_SCRIPT
 exit 0
