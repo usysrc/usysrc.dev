b4462492cd9a5a464775ff2a065e293e86af5607
usysrc <tilmann.hars@googlemail.com>
2021-01-28 22:17:22 +0100

    content

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 ## add a new page
 add a .html page to the pages directory
+
+### images
+
+## title images
+title images are 900x550
+they should have the id="titleimage"
+
+```
+<img src="img/music.jpg" id="titleimage">
+```
+
+## other images
+other images are ???
--- build.sh
+++ build.sh
@@ -3,14 +3,11 @@
 rm -rf ./dist/*
 
 mkdir dist/img/
-
 cp -r img/*.png ./dist/img/
 jpegoptim --size=100k img/*.jpg --dest ./dist/img/
 mogrify -strip ./dist/img/*.jpg
 cp -r style ./dist/
 
 for filename in ./pages/*.html; do
-    # echo $filename
-    # filename=$(basename $filename)
     cat header.html $filename footer.html > ./dist/$(basename $filename)
 done
